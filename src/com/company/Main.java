b6fffe28c7e0db5f5e9282b504ac111f9d459b3e
HariP5 <hari.pokhrel.2020@student.lhschools.org>
2019-10-28 20:36:48 -0400

    Added instrutions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,23 @@
 package com.company;
 
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner keyboard = new Scanner(System.in);
+        System.out.println("You will have to choose a number in the beginning before you play the game");
+        System.out.println("Then you will try to guess the number from the range of the number you picked");
+        System.out.println("Pick a number: ");
 
+
+        Scanner keyboard = new Scanner(System.in);
+        int maximum;
+        maximum = keyboard.nextInt();
+
+        Random rand = new Random();
 
 
 
